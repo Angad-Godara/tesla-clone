024fbc450140c86c1963135f2a49e7146f86a170
Angad-Godara <angadrrr2002@gmail.com>
2022-05-15 20:35:08 +0530

    ADD: redux for cars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './component/Header';
 import Home from './component/Home';
@@ -8,7 +7,7 @@ import Home from './component/Home';
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
     </div>
   );
---
+++ src/features/car/CarSlice.js
@@ -0,0 +1,16 @@
+import {createSlice} from "@reduxjs/toolkit";
+//  slice is just like a slice of a pizza
+
+const initialState = {
+    cars: ["Model X", "Model 3", "Model S", "Model Y"]
+}
+
+const CarSlice = createSlice({
+    name: "car",
+    initialState,
+    reducers:{}
+})
+
+export const selectCars = state => state.car.cars; // this statement is directly mapping to line no. 5
+
+export default CarSlice.reducer;
